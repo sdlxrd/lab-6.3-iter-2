79cfd7d79ca4f6cda04d33c9729c73cf74352d8d
sdlxrd <verysadforever26@gmail.com>
2021-11-04 10:42:19 +0200

    Добавьте файлы проекта.

---
+++ lab 6.3(iter) 2/lab 6.3(iter) 2.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <iomanip>
+
+// з використанням функцій
+
+using namespace std;
+
+void Input(int* a, const int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << "a[" << i << "] = ";
+		cin >> a[i];
+	}
+	cout << endl;
+}
+void Print(int* a, const int size)
+{
+	cout << "a = {";
+	for (int i = 0; i < size; i++)
+		cout << setw(4) << a[i];
+	cout << "}" << endl;
+}
+int Count(int* a, const int size)
+{
+	int count = 0;
+	for (int i = 0; i < size; i++)
+		if (a[i] < 0)
+			count++;
+	return count;
+}
+
+int main()
+{
+	int n;
+	cout << "n = "; cin >> n;
+	int* a = new int[n];
+
+	Input(a, n);
+	Print(a, n);
+
+	cout << "Count = " << Count(a, n) << endl;
+
+	return 0;
+}
